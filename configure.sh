44c7c4b71c7cbc5626dab63b2d5a9b841c7f6e31
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-07-02 19:19:35 -0300

    chore: add a else case to the config script

--- configure.sh
+++ configure.sh
@@ -6,5 +6,7 @@ for folder in $(echo "$CHECK_FOLDERS" | sed "s/,/ /g"); do
   if [ ! -d "$folder" ]; then
     echo "Creating $folder..."
     mkdir "$folder"
+  else
+    echo "Nothing do do."
   fi
 done
